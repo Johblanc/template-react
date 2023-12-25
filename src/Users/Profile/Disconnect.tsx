48c4a8ab5d0af7b2f8ff20cc7a65f061669f3f2b
Johblanc <79146321+Johblanc@users.noreply.github.com>
2023-12-25 19:05:27 +0100

    feat : disconnect

---
+++ src/Users/Profile/Disconnect.tsx
@@ -0,0 +1,16 @@
+import { useContext } from "react";
+import { TokenContext } from "../../Context/TokenContext";
+import { UserContext } from "../../Context/UserContext";
+import { USER_DEFAULT } from "../../Context/UserDefault";
+
+export function Disconnect() {
+  const { setUser } = useContext(UserContext);
+  const { setToken } = useContext(TokenContext);
+
+  const handleDisconnect = () => {
+    setUser(USER_DEFAULT);
+    setToken("");
+  };
+
+  return <button onClick={handleDisconnect}>Deconnection</button>;
+}
